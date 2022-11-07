4d8ea70a77e1cfdd26755a986b87538bb5038b6d
davimc <davimatosc@hotmail.com>
2022-11-06 22:26:27 -0300

    exception handling

---
+++ src/main/java/com/devsuperior/movieflix/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,72 @@
+package com.devsuperior.movieflix.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.devsuperior.movieflix.services.exceptions.DatabaseException;
+import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
+import com.devsuperior.movieflix.services.exceptions.ObjectNotFoundException;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError err = new StandardError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Database exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError err = new ValidationError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        e.getBindingResult().getFieldErrors().stream()
+                .forEach(f -> err.addError(f.getField(),f.getDefaultMessage()));
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<OAuthError> forbidden(ForbiddenException e, HttpServletRequest request){
+        OAuthError err = new OAuthError("Forbidden",  e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<OAuthError> unauthorized(UnauthorizedException e, HttpServletRequest request){
+        OAuthError err = new OAuthError("Unauthorized",  e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
+    }
+}
---
+++ src/main/java/com/devsuperior/movieflix/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.devsuperior.movieflix.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(Long id, Class<?> categoryClass) {
+        super("Object not found. id: " + id + " to: " + categoryClass.getSimpleName());
+    }
+}
