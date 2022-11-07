b70bb56b68b61ec07fe4acee38c09872fbe43098
davimc <davimatosc@hotmail.com>
2022-11-06 22:39:38 -0300

    configuration (security, authentication, authorization)

---
+++ src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
@@ -0,0 +1,68 @@
+package com.devsuperior.movieflix.dto;
+
+import com.devsuperior.movieflix.entities.User;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    @Size(min=3, max=60, message="Deve ter entre 5 e 60 caracteres")
+    @NotBlank(message="Campo requerido")
+    private String name;
+    @Email(message = "Favor entrar um e-mail válido")
+    private String email;
+    private Set<RoleDTO> roles = new HashSet<>();
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String firstName, String email) {
+        this.id = id;
+        this.name = firstName;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        roles = entity.getRoles().stream().map(RoleDTO::new).collect(Collectors.toSet());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }
+
+}
